c6b99cd5888da6c7a314f55ce098bc922da4cdfd
flebuster <flebuster@tut.by>
2021-03-22 17:08:52 +0300

    1

--- js/sript.js
+++ js/sript.js
@@ -5,3 +5,5 @@ for (var i = 0; i < document.body.children.length; i++) {
     alert( document.body.childNodes[i] ); 
   }
 
+console.log(document.body.children.length);
+console.log(document.body.childNodes.length);
